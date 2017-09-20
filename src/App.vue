6120dd312eae31fb89ab99e8992ae36a9db650de
adyakbar <muh.adyakbar@gmail.com>
2017-09-20 13:55:36 +0700

    belajar membangun vue gallery dengan vue webpack

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+export default {
+  name: 'app',
+  components: { Navbar }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ImageList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a href="#" @click="emitImage(image)" v-for="image in images">
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img :src="image.thumbnail" alt="" class="img-responsive">
+      </div>
+    </a>
+  </div>  
+</template>
+
+<script>
+export default {
+  props: ['images'],
+  methods: {
+    emitImage: function (image) {
+      this.$emit('emitImage', image)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
